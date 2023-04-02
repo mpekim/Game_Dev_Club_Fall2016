aa0645f3a05f162013484446008f823e3c69820e
mpekim_wsu <mpmorley@worcester.edu>
2023-04-01 20:13:17 -0400

    Modifying bash script to build Docker container.

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,4 @@
 # Empty Bash Script File. Do research on how to make Bash Scripts.
-# Add directory for Docker commands.
-docker start -a
+# Add directory for Docker commands. (Not needed!)
+docker build -t game_dev .
+docker run --name game_dev game_dev

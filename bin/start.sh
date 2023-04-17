c7100f5b375827589a764a49f4c01dcd73d668e5
mpekim_wsu <mpmorley@worcester.edu>
2023-04-16 21:56:36 -0400

    Minor changes to start script.

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,6 @@
 # Empty Bash Script File. Do research on how to make Bash Scripts.
 # Add directory for Docker commands. (Not needed!)
+
+echo "Starting container in interactive mode..."
 docker start -i game_dev
+# Start the container in interactive mode.

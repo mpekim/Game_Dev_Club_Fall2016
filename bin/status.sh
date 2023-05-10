d8aee6995482a95af19a59eca81cea05674bc824
mpekim_wsu <mpmorley@worcester.edu>
2023-05-09 22:11:45 -0400

    Almost done with this semester! Final Project presentation tomorrow.

--- bin/status.sh
+++ bin/status.sh
@@ -1,4 +1,5 @@
 # Empty Bash Script File. Do research on how to make Bash Scripts.
 # Find out better way to make status script.
+echo "Checking status of containers and images..."
 docker ps -a
 docker images

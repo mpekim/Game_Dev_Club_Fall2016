de565025a8bf2999ae40186167e0a5dc0fccbb02
mpekim_wsu <mpmorley@worcester.edu>
2023-10-21 18:44:30 -0400

    Auto-commit via update.sh script.

--- bin/update.sh
+++ bin/update.sh
@@ -1,4 +1,6 @@
 # Bash script to perform all commit steps in one go.
+
+$branch_name = "master"
 echo "Preparing to update latest commit..."
 
 git add .
@@ -7,6 +9,6 @@ echo "Files added."
 git commit -m "Auto-commit via update.sh script."
 echo "Files committed."
 
-git pull origin master
-git push origin master
+git pull origin $branch_name
+git push origin $branch_name
 echo "Auto-update successful."

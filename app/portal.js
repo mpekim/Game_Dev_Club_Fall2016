3b84f3de7983ec358bb76515c9abcbfb68ca353d
mpekim_wsu <mpmorley@worcester.edu>
2023-10-21 07:40:26 -0400

    NEW FILES: HTML file for User creation; Portal file for tracking traversal JS functions.

---
+++ app/portal.js
@@ -0,0 +1,57 @@
+// JS file for travel between different files.
+
+// All level functions have the same structure.
+// Pre-Condition: These functions accepts no arguments.
+// Post-Condition: All functions will go to their intended
+// locations.
+function goToLevel1(){
+    var url = "level1.html";
+    window.location.href = url;
+}
+
+function goToLevel2(){
+    var url = "level2.html";
+    window.location.href = url;
+}
+
+function goToLevel3(){
+    var url = "level3.html";
+    window.location.href = url;
+}
+
+function goToLevel4(){
+    var url = "level4.html";
+    window.location.href = url;
+}
+
+function endGame(){
+    var url = "endGame.html";
+    window.location.href = url;
+}
+
+function goToWrong()
+{
+    var url = "wrong.html";
+    window.location.href = url;
+}
+
+function startProgram(){
+    // alert("Testing to ensure that the function works as intended.");
+     var url = "index.html";
+     window.location.href = url;
+ }
+ 
+ function returnToIndex(){
+     var url = "index.html";
+     window.location.href = url;
+ }
+ 
+ function goToPasswordInsert(){
+     var url = "passwordInsert.html";
+     window.location.href = url;
+ }
+
+ function goToCreateUser(){
+    var url = "createUser.html";
+    window.location.href = url;
+ }
